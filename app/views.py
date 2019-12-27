2a881d3bc6a9e6403c529c3afe4aa10b1d48c844
KDragon <elkarouani@gmail.com>
2019-12-27 22:11:08 +0100

    Task: linking the view with the url patterns on urls.py

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect
+from django.shortcuts import render
 from django.core.mail import send_mail
 from django.conf import settings
 
@@ -6,8 +6,8 @@ def email(request):
     subject = 'Thank you for registering to our site'
     message = ' it means a world to us '
     email_from = settings.EMAIL_HOST_USER
-    recipient_list = ['receiver@gmail.com',]
+    recipient_list = ['kdragondev@gmail.com',]
 
-    send_mail( subject, message, email_from, recipient_list )
+    send_mail(subject=subject, message=message, from_email=email_from, recipient_list=recipient_list)
 
-    return redirect('redirect to a new page')
+    return render('redirect to a new page')
