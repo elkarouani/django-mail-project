f64ceba131b70d28d3273314dc4c7aea4a3ad891
KDragon <elkarouani@gmail.com>
2019-12-27 21:27:44 +0100

    Task: create email view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.core.mail import send_mail
+from django.conf import settings
 
-# Create your views here.
+def email(request):
+    subject = 'Thank you for registering to our site'
+    message = ' it means a world to us '
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = ['receiver@gmail.com',]
+
+    send_mail( subject, message, email_from, recipient_list )
+
+    return redirect('redirect to a new page')
--- src/settings.py
+++ src/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'kdragondev@gmail.com'
+EMAIL_HOST_PASSWORD = 'salah-kara'
